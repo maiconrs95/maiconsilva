c3347f77ab24a7acd2b452dd411cd58677254008
Maicon Silva <maiconrs95@gmail.com>
2019-11-16 02:02:54 -0300

    add: manifest and favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,18 +75,18 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Maicon Silva`,
+                short_name: `Maicon Silva`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#333333`,
+                theme_color: `#333333`,
                 display: `minimal-ui`,
-                // icon: `src/images/gatsby-icon.png`,
+                icon: `src/images/favicon-16x16.png`,
             },
         },
         `gatsby-plugin-sitemap`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        `gatsby-plugin-offline`,
     ],
 }
