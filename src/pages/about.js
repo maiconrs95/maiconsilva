f4cfe1252fc51e4c9fc23cc8f6fd1e77a5e957d9
Maicon Silva <maiconrs95@gmail.com>
2020-04-07 00:32:13 -0300

    update: about

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,21 +16,18 @@ function AboutPage() {
             <MainContent>
                 <h1>Sobre mim</h1>
                 <p>
-                    Meu nome é Maicon Rocha da Silva, 24, nasci em São Paulo/SP e
-                    atualmente sou Frontend Developer na{' '}
-                    <a href="https://ingresse.com" target="_blank" rel="noopener noreferrer">
-                        Ingresse
-                    </a>
-                    .
+                    Eu sou Maicon, 24, from Brazil/SP e desenvolvedor web.
+
+                    Criei esse blog a fim de documentar e compartilhar tudo o que eu aprendo.
                 </p>
 
                 <p>
-                    Cursei Análise e Desenvolvimento de Sistemas na Universidade Mogi das Cruzes entre 2016 - 2018.
+                    Me formei em Análise e Desenvolvimento de Sistemas na UMC, em 2018.
                 </p>
 
                 <p>
-                    Escolhi a área de tecnologia, pois sempre me identifiquei. E quanto mais eu aprendo sobre essa área, mais
-                    feliz eu fico com a escolha que fiz.
+                    Escolhi o desenvolvimento pelo "poder da criação" e quanto mais eu procuro aprender sobre essa área, mais apaixonado
+                    eu fico.
                 </p>
 
                 <h2>Habilidades</h2>
@@ -38,15 +35,14 @@ function AboutPage() {
                 <ul>
                     <li>HTML</li>
                     <li>CSS (Stylus, Sass, Less, PostCSS)</li>
-                    <li>Javascript (Design Patterns, ES6/7+)</li>
+                    <li>JavaScript (Design Patterns, ES6/7+)</li>
                     <li>Design Responsivo (Mobile First)</li>
                     <li>Css Frameworks</li>
-                    <li>ReactJS / Redux / Flux</li>
+                    <li>ReactJS / React Native / Redux / Flux</li>
                     <li>NodeJS</li>
                     <li>Git</li>
                     <li>MySQL - MongoDB</li>
                     <li>Scrum</li>
-                    <li>E o que não estiver na lista, dou um jeito de aprender!</li>
                 </ul>
 
                 <h2>Contato</h2>
