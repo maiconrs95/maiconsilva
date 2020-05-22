a95b3e0fba0490a64758d29fecc806d67bb5de50
Maicon Silva <maiconrs95@gmail.com>
2020-05-21 23:51:17 -0300

    update post items

--- src/components/PostItem/styles.js
+++ src/components/PostItem/styles.js
@@ -4,21 +4,15 @@ import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 export const PostItemWrapper = styled.section`
     display: flex;
-    align-items: center;
     border-bottom: 1px solid var(--borders);
-    padding: 2rem 3rem;
+    align-items: flex-start;
     width: 100%;
-
-    body#grid & {
-        border: none;
-        padding: 2rem 1rem;
-        flex-direction: column;
-        justify-content: center;
-    }
+    border: none;
+    padding: 2rem 3rem;
+    flex-direction: column;
+    justify-content: center;
 
     ${media.lessThan("large")`
-        align-items: flex-start;
-        flex-direction: column;
         padding: 2rem 1rem;
     `}
 `;
@@ -41,37 +35,20 @@ export const PostItemTag = styled.div`
     display: flex;
     align-items: center;
     background: ${(props) => props.background};
-    border-radius: 50%;
     color: #fff;
-    font-size: 1.3rem;
+    font-size: 1rem;
     font-weight: 700;
     justify-content: center;
-    min-height: 90px;
-    min-width: 90px;
+    min-height: auto;
+    min-width: auto;
     text-transform: uppercase;
-
-    ${media.lessThan("large")`
-        border-radius: 0;
-        font-size: 1rem;
-        min-height: auto;
-        min-width: auto;
-        padding: .2rem .5rem;
-        margin-bottom: .7rem;
-    `}
-
-    body#grid & {
-        margin-bottom: 1.5rem;
-    }
+    padding: .2rem .5rem;
+    margin-bottom: 1.5rem;
 `;
 
 export const PostItemInfo = styled.div`
     display: flex;
     flex-direction: column;
-    margin-left: 1.5rem;
-
-    ${media.lessThan("large")`
-        margin: 0;
-    `}
 `;
 
 export const PostItemDate = styled.time`
