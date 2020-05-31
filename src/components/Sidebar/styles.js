ad269be47d2733cb4a5b4a2220e7f086cd289e31
Maicon Silva <maiconrs95@gmail.com>
2020-05-30 23:36:06 -0300

    update: desktop sidebar color

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -6,7 +6,6 @@ export const SidebarWrapper = styled.aside`
     flex-direction: column;
     align-items: center;
     border-right: 1px solid var(--borders);
-    background-color: var(--mediumBackground);
     height: 100vh;
     position: fixed;
     padding: 2rem;
